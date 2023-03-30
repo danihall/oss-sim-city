6a336a038549fb6b58d0fa2585e7fc580a8d89d6
danihall <danielhalle82@gmail.com>
2023-03-30 18:32:38 +0200

    add support for extended interfaces

--- bin/styleguide-automator/index.mjs
+++ bin/styleguide-automator/index.mjs
@@ -12,7 +12,9 @@ import {
 import { createExportStatement } from "./utils/exportTemplate.mjs";
 import { COMPONENTS_PATH, STYLEGUIDE_PATH } from "./utils/paths.mjs";
 
-const REGEX_INTERFACE = /(?<=interface\s)[^\s]+/;
+const REGEX_INTERFACE = /(?<=interface\s)([aA-zZ]|[\s](?!{))+/;
+const EXTENDS = " extends ";
+const TSX = ".tsx";
 
 /**
  * @see /utils/sourceOfTruth.mjs
@@ -54,6 +56,10 @@ const _mapToSourceOfTruthContext = function (content_entry) {
 };
 
 /**
+ * Some "meta-programming" is done when using Object.defineProperty().
+ * Getters and functions are added to SOURCE_OF_TRUTH object.
+ * When applying JSON.stringify() on SOURCE_OF_TRUTH, getters will be accessed and functions will then be discarded.
+ * This is ideal since this gives a json file with values created dynamically.
  * @param {string} file
  * @returns {Promise}
  */
@@ -61,20 +67,26 @@ const _updateSourceOfTruth = async (file) => {
   const content = await fs.promises.readFile(file, "utf-8");
   const content_as_array = content.split("\n");
 
-  const component_name = file.split("/").pop().replace(".tsx", "");
-  const path = file.replace(".tsx", "");
-  const _props = [];
-  let interface_name = null;
+  const component_name = file.split("/").pop().replace(TSX, "");
+  const path = file.replace(TSX, "");
+  const props_list = [];
+  let interface_name = undefined;
+  let extended_interface = false;
 
   parent_loop: for (let i = 0; i < content_as_array.length; i++) {
-    interface_name = content_as_array[i].match(REGEX_INTERFACE)?.[0];
+    const interface_match /*: string[] | undefined*/ = content_as_array[i]
+      .match(REGEX_INTERFACE)?.[0]
+      .split(EXTENDS);
 
-    if (interface_name) {
+    if (interface_match) {
+      interface_name = interface_match[0];
+      extended_interface = interface_match.length > 1 && interface_match[1];
+
       for (let j = i + 1; j < content_as_array.length; j++) {
         if (content_as_array[j] !== "}") {
           let [key, value] = content_as_array[j].split(":");
 
-          _props.push([
+          props_list.push([
             key.replace("?", "").trim(),
             value.replace(";", "").trim(),
           ]);
@@ -85,11 +97,13 @@ const _updateSourceOfTruth = async (file) => {
     }
   }
 
-  if (interface_name && _props.length) {
+  if (interface_name && props_list.length) {
     Object.defineProperty(SOURCE_OF_TRUTH, interface_name, {
       enumerable: true,
       value: function () {
-        return Object.fromEntries(_props.map(_mapToSourceOfTruthContext, this));
+        return Object.fromEntries(
+          props_list.map(_mapToSourceOfTruthContext, this)
+        );
       },
     });
   }
@@ -99,9 +113,10 @@ const _updateSourceOfTruth = async (file) => {
     get() {
       const _self = this; //eslint-disable-line
       return {
-        path,
         get props() {
-          return _self[interface_name]?.();
+          const extended_props = _self[extended_interface]?.() ?? {};
+          const props = _self[interface_name]?.();
+          return { ...extended_props, ...props };
         },
       };
     },
@@ -120,7 +135,7 @@ const _setComponentSpecs = async (component_folder) => {
   );
 
   const component_files = files
-    .filter((file) => path.extname(file) === ".tsx")
+    .filter((file) => path.extname(file) === TSX)
     .map((tsx_file) => `${COMPONENTS_PATH}/${component_folder}/${tsx_file}`);
 
   return Promise.all(component_files.map(_updateSourceOfTruth));
